db2bc953cf375a70bb881d4c504509eccf350893
ferm <fermandrei1@gmail.com>
2025-11-09 01:33:10 +0800

    added MainConten and RightSideBar import component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import LeftSidebar from "./components/LeftSidebar";
+import MainContent from "./components/MainContent";
+import RightSidebar from "./components/RightSidebar";
 
 function App() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex justify-between min-h-screen">
       <LeftSidebar />
+      <MainContent />
+      <RightSidebar />
     </div>
   );
 }
