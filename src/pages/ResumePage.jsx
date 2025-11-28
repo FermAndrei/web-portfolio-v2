794781dfda7dd2240d00b7e225611ebe23109dea
ferm <fermandrei1@gmail.com>
2025-11-28 20:08:34 +0800

    fixed the Resume

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -3,21 +3,32 @@ import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { Button } from "@mui/material";
+import { MdDataArray } from "react-icons/md";
 
 const ResumePage = () => {
   return (
-    <div className="w-full">
-      <div className="bg-black/50 flex items-center justify-center">
-        <div className="bg-white rounded-lg shadow-lg w-[90%] h-[90%] relative flex flex-col">
-          <div className="flex-1 overflow-hidden mt-6">
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-              <Viewer
-                fileUrl="/Trillana, Ferm Andrei - Resume.pdf"
-                theme="light"
-              />
-            </Worker>
-          </div>
-        </div>
+    <div className="w-full p-3 flex flex-col">
+      <div className="flex mb-3 justify-between items-center">
+        <h2 className="flex items-center gap-2 text-white text-xl font-semibold">
+          <span>
+            <MdDataArray />
+          </span>
+          Resume
+        </h2>
+        <a
+          href="https://drive.google.com/uc?export=download&id=1rQ9eEweGhFB0FU49CMjiqW-GZVIm0TpA"
+          download="Ferm-Andrei-Trillana-Resume.pdf"
+        >
+          <Button variant="contained" color="primary">
+            Download
+          </Button>
+        </a>
+      </div>
+
+      <div className="overflow-hidden rounded">
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+          <Viewer fileUrl="/Trillana, Ferm Andrei - Resume.pdf" />
+        </Worker>
       </div>
     </div>
   );
